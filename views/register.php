3e29948f2eaca9f422efcaf24a4fbaf9f6e7ebda
RuthStam <r.g.b.stam@student.rug.nl>
2020-12-13 14:09:45 +0100

    Made the templates for the views

--- views/register.php
+++ views/register.php
@@ -1,1 +1,100 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Own CSS -->
+    <!--<link rel="stylesheet" href="/DDWT20/week2/css/main.css">-->
+
+    <title><?= 'page_title'//$page_title ?></title>
+</head>
+<body>
+<!-- Menu -->
+<?= 'navigation'//$navigation ?>
+
+<!-- Content -->
+<div class="container">
+    <!-- Breadcrumbs -->
+    <div class="pd-15">&nbsp</div>
+    <?= 'breadcrumbs'//$breadcrumbs ?>
+
+    <div class="row">
+
+        <!-- Left column -->
+        <div class="col-md-12">
+            <!-- Error message -->
+            <?php //if (isset($error_msg)){echo $error_msg;} ?>
+
+            <h1><?= 'page_title'//$page_title ?></h1>
+            <h5><?= 'page_title'//$page_subtitle ?></h5>
+
+            <div class="pd-15">&nbsp;</div>
+
+            <form action="/DDWT20_finalproject/register/" method="POST">
+                <div class="form-group">
+                    <label for="inputUsername">Username</label>
+                    <input type="text" class="form-control" id="inputUsername" placeholder="j.jansen" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input type="password" class="form-control" id="inputPassword" placeholder="******" name="password" required>
+                </div>
+                <div class="form-group">
+                    <label for="inputFullname">Full name</label>
+                    <input type="text" class="form-control" id="inputFullname" placeholder="Jan Jansen" name="fullname" required>
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail">Email</label>
+                    <input type="email" class="form-control" id="inputEmail" placeholder="j.jansen@email.com" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="inputPhonenumber">Phone number</label>
+                    <input type="tel" class="form-control" id="inputPhonenumber" placeholder="0612345678" name="phonenumber" required>
+                </div>
+                <div class="form-group">
+                    <label for="inputBirthdate">Birth date</label>
+                    <input type="date" class="form-control" id="inputBirthdate" placeholder="YYYY-MM-DD" name="birthdate" required>
+                </div>
+                <div class="form-group">
+                    <label for="inputLanguage">Language</label>
+                    <input type="text" class="form-control" id="inputLanguage" placeholder="Dutch" name="language" required>
+                </div>
+                <div class="form-group">
+                    <label for="inputStudyProfession">Study/Profession</label>
+                    <input type="text" class="form-control" id="inputStudyProfession" placeholder="Information Science" name="studyprofession" required>
+                </div>
+                <div class="form-group">
+                    <label for="inputBiography">Biography</label>
+                    <input type="text" class="form-control" id="inputBiography" placeholder="Bio" name="biography" required>
+                </div>
+                <div class="form-group">
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="tenant" value="tenant">
+                        <label class="form-check-label" for="tenant">I want to rent a room</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="owner" value="owner">
+                        <label class="form-check-label" for="owner">I want to rent out a room</label>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Register now</button>
+            </form>
+
+        </div>
+
+    </div>
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
--- views/room.php
+++ views/room.php
@@ -1,1 +1,116 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Own CSS -->
+    <!--<link rel="stylesheet" href="/DDWT20/week2/css/main.css">-->
+
+    <title><?= 'page_title'//$page_title ?></title>
+</head>
+<body>
+<!-- Menu -->
+<?= 'navigation'//$navigation ?>
+
+<!-- Content -->
+<div class="container">
+    <!-- Breadcrumbs -->
+    <div class="pd-15">&nbsp</div>
+    <?= 'breadcrumbs'//$breadcrumbs ?>
+
+    <div class="row">
+
+        <!-- Left column -->
+        <div class="col-md-8">
+            <!-- Error message -->
+            <?php //if (isset($error_msg)){echo $error_msg;} ?>
+
+            <h1><?= 'page_title'//$page_title ?></h1>
+            <h5><?= 'page_subtitle'//$page_subtitle ?></h5>
+            <p><?= 'page_content'//$page_content ?></p>
+            <?php //if ($display_buttons) { if your the owner ?>
+                <div class="row">
+                    <div class="col-sm-2">
+                        <a href="/DDWT20_finalproject/edit/?serie_id=<?=
+                        'room_id'//$serie_id ?>" role="button" class="btn btn-warning">Edit</a>
+                    </div>
+                    <div class="col-sm-2">
+                        <form action="/DDWT20_finalproject/remove/"
+                              method="POST">
+                            <input type="hidden" value="<?= 'room_id'//$serie_id
+                            ?>" name="serie_id">
+                            <button type="submit" class="btn btndanger">Remove</button>
+                        </form>
+                    </div>
+                </div>
+            <?php //} ?>
+            <table class="table">
+                <tbody>
+                <tr>
+                    <th scope="row">Price</th>
+                    <td><?= 'price'//$creators ?></td>
+                </tr>
+                <tr>
+                    <th scope="row">Type</th>
+                    <td><?= 'type'//$nbr_seasons ?></td>
+                </tr>
+                <tr>
+                    <th scope="row">Size</th>
+                    <td><?= 'size'//$added_by ?></td>
+                </tr>
+                <tr>
+                    <th scope="row">Owner</th>
+                    <td><?= 'owner'//$added_by ?></td>
+                </tr>
+                <tr>
+                    <th scope="row">Address</th>
+                    <td><?= 'Street housenumber'//$added_by ?></td>
+                </tr>
+                <tr>
+                    <th scope="row">Zipcode</th>
+                    <td><?= 'zipcode'//$added_by ?></td>
+                </tr>
+                <tr>
+                    <th scope="row">City</th>
+                    <td><?= 'city'//$added_by ?></td>
+                </tr>
+
+                </tbody>
+            </table>
+            <!--<div class="row">
+                <div class="col-sm-2">
+                    <a href="/DDWT20_finalproject/edit/?serie_id=<?= 'room_id'//$serie_id ?>" role="button" class="btn btn-warning">Edit</a>
+                </div>
+                <div class="col-sm-2">
+                    <form action="/DDWT20_finalproject/remove/" method="POST">
+                        <input type="hidden" value="<?= 'room_id'//$serie_id ?>" name="serie_id">
+                        <button type="submit" class="btn btn-danger">Remove</button>
+                    </form>
+                </div>
+            </div>-->
+
+        </div>
+
+        <!-- Right column -->
+        <div class="col-md-4">
+
+            <?php //include $right_column ?>
+
+        </div>
+
+    </div>
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>

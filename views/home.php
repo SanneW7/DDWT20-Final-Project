364ec3970cf0f729249bff879aef92805e040db7
MichielPronk <michiel_pronk@upcmail.nl>
2021-01-07 16:00:17 +0100

    Icon added

--- views/home.php
+++ views/home.php
@@ -2,14 +2,17 @@
 <html lang="en">
     <head>
         <!-- Required meta tags -->
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Bootstrap CSS -->
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
         <!-- Own CSS -->
-            <link rel="stylesheet" href="/DDWT20-Final-Project/css/main.css">
+        <link rel="stylesheet" href="/DDWT20-Final-Project/css/main.css">
+
+        <!-- Icon -->
+        <link rel="shortcut icon" type="image/x-icon" href="css/favicon.ico">
 
         <!-- Google fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
